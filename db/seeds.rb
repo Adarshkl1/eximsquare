4cd725c78cdb7b30b31a3088447c3dd0f5968905
eximsq <eximsquare.help@gmail.com>
2023-05-20 19:30:21 +0530

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@ Exportnew.create(source: "GLOBAL COMPLIANCE NEWS",
 title: "China is set to review the trade remedies measures on Australia barley",
 name: "The Ministry of Commerce of China (“MOFCOM“) initiated an antidumping and countervailing duty administrative review of Barley from Australia, effective on...",
 link: "https://www.globalcompliancenews.com/2023/04/22/https-www-internationaltradecomplianceupdate-com-2023-04-20-china-is-set-to-review-the-trade-remedies-measures-on-australia-barley-_04202023/")
+
+
+Exportnew.create(source: "GLOBAL COMPLIANCE NEWS",
+title: "China is set to review the trade remedies measures on Australia barley",
+name: "The Ministry of Commerce of China (“MOFCOM“) initiated an antidumping and countervailing duty administrative review of Barley from Australia, effective on...",
+link: "https://www.globalcompliancenews.com/2023/04/22/https-www-internationaltradecomplianceupdate-com-2023-04-20-china-is-set-to-review-the-trade-remedies-measures-on-australia-barley-_04202023/")
